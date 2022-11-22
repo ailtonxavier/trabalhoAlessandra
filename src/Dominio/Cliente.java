fa48f2a52e4ace2121c3a8cfc7923375bbde7320
Ailton Xavier <ailton.xavier@hotmail.com.br>
2022-11-22 10:41:12 -0300

    ClienteDAO 50%

--- src/Dominio/Cliente.java
+++ src/Dominio/Cliente.java
@@ -1,20 +1,28 @@
 package Dominio;
 
 public class Cliente {
-    private int cpf;
+    private int pk_cpf;
     private String nome;
     private String login;
     private String senha;
     private boolean ativo;
 
+    public Cliente(int pk_cpf, String nome, String login, String senha, boolean ativo){
+        this.pk_cpf = pk_cpf;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.ativo = ativo;
+    }
+
     // Getters
-    public int getCpf(){return cpf;}
+    public int getCpf(){return pk_cpf;}
     public String getNome(){return nome;}
     public String getLogin(){return login;}
     public String getSenha(){return senha;}
     public boolean getAtivo(){return ativo;}
     // Setters
-    public void setCpf(int cpf){this.cpf = cpf;}
+    public void setCpf(int pk_cpf){this.pk_cpf = pk_cpf;}
     public void setNome(String nome){this.nome = nome;}
     public void setLogin(String login){this.login = login;}
     public void setSenha(String senha){this.senha = senha;}
--- src/Persistencia/ClienteDAO.java
+++ src/Persistencia/ClienteDAO.java
@@ -5,9 +5,28 @@ import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
+import Dominio.Cliente;
+
 public class ClienteDAO {
-    private Conexao conexaoContatoDao;
-    private String REL = "select * from \"Cliente\"";
+    private Conexao conexaoClienteDao;
+    private String RelatorioCliente = "select * from \"Cliente\"";
     private String BUS = "select * from \"Cliente\" where \"pk_cpf\"=?";
 
+    public ArrayList<Cliente> emitirRelatorioClientes(){
+        Cliente pessoa;
+        ArrayList<Cliente> lista = new ArrayList<Cliente>();
+        try {
+            conexaoClienteDao.conectar();
+            Statement instrucao = conexaoClienteDao.getConexao().createStatement();
+            ResultSet rs = instrucao.executeQuery(RelatorioCliente);
+            while(rs.next()){
+                pessoa = new Cliente(rs.getInt("pk_cpf"), rs.getString("nome"), rs.getString("senha"), rs.getString("login"), rs.getBoolean("ativo"));
+                lista.add(pessoa);
+            }
+            conexaoClienteDao.desconectar();
+        } catch (Exception e){
+            System.out.println();
+        }
+        return lista;
+    }
 }
