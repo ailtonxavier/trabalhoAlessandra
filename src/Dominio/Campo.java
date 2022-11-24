1019819167eefaf9b0a7cf4f59203edea07343a0
alefe-lemuel <awlef.2003@gmail.com>
2022-11-24 00:39:33 -0300

    CampoDAO iniciado

--- src/Dominio/Campo.java
+++ src/Dominio/Campo.java
@@ -1,25 +1,25 @@
 package Dominio;
 
 public class Campo {
-    private int fk_pk_id_aluguel;
+    private int fk_pk_idAluguel;
     private static final int valorHora = 50;
     private boolean ocupado;
     private int capacidade;
 
-    public Campo(int fk_pk_id_aluguel, int valorHora, boolean ocupado, int capacidade){
-        this.fk_pk_id_aluguel = fk_pk_id_aluguel;
+    public Campo(int fk_pk_idAluguel, int valorHora, boolean ocupado, int capacidade){
+        this.fk_pk_idAluguel = fk_pk_idAluguel;
         this.ocupado = ocupado;
         this.capacidade = capacidade;
     }
 
     //getters
-    public int getFk_pk_id_aluguel() {return fk_pk_id_aluguel;}
+    public int getFk_pk_idAluguel() {return fk_pk_idAluguel;}
     public static int getValorHora() {return valorHora;}
     public boolean isOcupado() {return ocupado;}
     public int getCapacidade() {return capacidade;}
 
     //setters
-    public void setFk_pk_id_aluguel(int fk_pk_id_aluguel) {this.fk_pk_id_aluguel = fk_pk_id_aluguel;}
+    public void setFk_pk_idAluguel(int fk_pk_idAluguel) {this.fk_pk_idAluguel = fk_pk_idAluguel;}
     public void setCapacidade(int capacidade) {this.capacidade = capacidade;}
     public void setOcupado(boolean ocupado) {this.ocupado = ocupado;}
 }
--- src/Persistencia/CampoDAO.java
+++ src/Persistencia/CampoDAO.java
@@ -1,4 +1,34 @@
 package Persistencia;
 import Dominio.Campo;
+
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class CampoDAO {
+    private Conexao conexaoCampoDao;
+    private String RelatorioCampos = "select * from \"Campo\"";
+
+    public CampoDAO(){
+        conexaoCampoDao = new Conexao("postgres","123","jdbc:postgresql://localhost:5432/postgres");
+    }
+
+    public ArrayList<Campo> emitirRelatorioCampos(){
+        Campo terreno;
+        ArrayList<Campo> lista = new ArrayList<Campo>();
+        try {
+            conexaoCampoDao.conectar();
+            Statement instrucao = conexaoCampoDao.getConexao().createStatement();
+            ResultSet rs = instrucao.executeQuery(RelatorioCampos);
+            while(rs.next()){
+                terreno = new Campo(rs.getInt("fk_pk_idAluguel"),rs.getInt("valorHora"), rs.getBoolean("ocupado"), rs.getInt("capacidade"));
+                lista.add(terreno);
+            }
+            conexaoCampoDao.desconectar();
+        } catch (Exception e){
+            System.out.println("Erro no relatorio de Campos");
+        }
+        return lista;
+    }
 }
